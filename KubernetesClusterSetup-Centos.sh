dafadd920c1176014bf913651bd693ba30f97dcb
Md Umair <81945492+umairmd385@users.noreply.github.com>
2023-12-19 10:00:52 +0530

    Update KubernetesClusterSetup-Centos.sh

--- KubernetesClusterSetup-Centos.sh
+++ KubernetesClusterSetup-Centos.sh
@@ -13,7 +13,7 @@ else
     echo "Installing Docker"
     sudo yum install -y yum-utils device-mapper-persistent-data lvm2
     sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-    sudo yum install -y docker-ce-24.0.6-1.el7 docker-ce-cli-24.0.6-1.el7 containerd.io docker-compose-plugin wget curl
+    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin wget curl
     sudo systemctl daemon-reload
     sudo systemctl start docker
     sudo systemctl enable --now docker
