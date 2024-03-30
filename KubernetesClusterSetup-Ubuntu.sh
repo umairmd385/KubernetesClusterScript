dad955464ab14a2fdb0cade9d830dd3a6ea00c15
Md Umair <81945492+umairmd385@users.noreply.github.com>
2024-03-30 12:05:28 +0530

    Updated repository for kubeadm

--- KubernetesClusterSetup-Ubuntu.sh
+++ KubernetesClusterSetup-Ubuntu.sh
@@ -28,8 +28,8 @@ sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg git wget
 sudo mkdir -p /etc/apt/keyrings
 sudo chmod -R 755 /etc/apt/keyrings
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update -y
 sudo apt -y install kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
